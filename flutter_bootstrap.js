180b54f4c0f874a8d1600b62e894bd2ef1ceb8ed
xpwmaosldk <github-actions[bot]@users.noreply.github.com>
2026-06-19 04:53:47 +0000

    Update from https://github.com/xpwmaosldk/cldlotto/commit/2b0ee9d5091c9ecc79c3db3c2e7b060cc60ec4fd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,6 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187ed6","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"}]};
+_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
-_flutter.loader.load();
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "575753526" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+  }
+});
