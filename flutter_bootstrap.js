ef8795804f163980a5899b4f4b025e0c7451dac7
xpwmaosldk <github-actions[bot]@users.noreply.github.com>
2026-06-21 04:14:18 +0000

    Update from https://github.com/xpwmaosldk/cldlotto/commit/5b43a486a9482093d6f1b912b517256971794af0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3113792284" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "496059451" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
